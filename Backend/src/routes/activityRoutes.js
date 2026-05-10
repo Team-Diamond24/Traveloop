a25f8039b88752c9812bc5c18f726e6b36e34d82
RISHI RAJ <rishiraj14rr@gmail.com>
2026-05-10 16:10:49 +0530

    Delete Backend/src directory

--- Backend/src/routes/activityRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const activityController = require('../controllers/activityController');
-
-router.get('/', activityController.listActivities);
-router.get('/:id', activityController.getActivityById);
-
-module.exports = router;
